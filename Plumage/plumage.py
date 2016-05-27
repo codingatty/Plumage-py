f11b858c024c8a7ecb6b72795f3038a653c0c0fd
Terry Carroll <carroll@tjc.com>
2016-05-27 12:28:58 -0700

    pylint: trailing whitespace fixes

--- Plumage/plumage.py
+++ Plumage/plumage.py
@@ -6,7 +6,7 @@ carroll@tjc.com
 
 License information:
 
-This program is licensed under Apache License, version 2.0 (January 2004), 
+This program is licensed under Apache License, version 2.0 (January 2004),
 http://www.apache.org/licenses/LICENSE-2.0
 
 Anyone who makes use of, or who modifies, this code is encouraged
@@ -52,7 +52,7 @@ class XSLTDescriptor(object):
         initialize a XSLTDescriptor object for the specified transform format
         '''
         xslt_dirname = _TSDR_dirname
-        xslt_filename = format+".xsl" 
+        xslt_filename = format+".xsl"
         xslt_pathname = os.path.join(xslt_dirname, xslt_filename)
         with open(xslt_pathname) as _f:
             _stylesheet = _f.read()
@@ -69,7 +69,7 @@ _TSDR_substitutions = {
     "$IMPLEMENTATIONNAME$":"Plumage-py",          # implementation identifier
     "$IMPLEMENTATIONVERSION$":__version__,      # implementation version no.
     "$IMPLEMENTATIONDATE$":__last_updated__,    # implementation date
-    "$IMPLEMENTATIONAUTHOR$":__author__ ,       # implementation author 
+    "$IMPLEMENTATIONAUTHOR$":__author__ ,       # implementation author
     "$IMPLEMENTATIONURL$":__URL__,              # implementation URL
     "$IMPLEMENTATIONCOPYRIGHT$": __copyright__, # implementation copyright notice
     "$IMPLEMENTATIONLICENSE$":__license__,      # implementation license
@@ -84,7 +84,7 @@ _xslt_table = {
     "ST66" : XSLTDescriptor("ST66"),
     "ST96" : XSLTDescriptor("ST96")
     }
-    
+
 class TSDRReq(object):
     '''
     TSDR request object
@@ -125,7 +125,7 @@ class TSDRReq(object):
         '''
         self.XSLT = template
         return
-    
+
     def unsetXSLT(self):
         '''
         Resets self.XSLT to None, causing the package-supplied templates to be used.
@@ -136,7 +136,7 @@ class TSDRReq(object):
     def setPTOFormat(self, format):
         '''
         Determines what format file will be fetched from the PTO.
-            "ST66": ST66-format XML 
+            "ST66": ST66-format XML
             "ST96": ST96-format XML
              "zip": zip file.  The zip file obtained from
                     the PTO is currently ST66-format XML.
@@ -147,7 +147,7 @@ class TSDRReq(object):
             raise ValueError("invalid PTO format '%s'" % format)
         self.PTOFormat = format
         return
-    
+
     def unsetPTOFormat(self):
         '''
         Resets PTO format to "zip" (default)
@@ -186,7 +186,7 @@ class TSDRReq(object):
         self.TSDRMap = None
         self.TSDRMapIsValid = False
         return
-    
+
     def getTSDRInfo(self, identifier=None, tmtype=None):
         '''
         Obtain XML trademark data from the PTO or local file, parse it to key/data pairs
@@ -213,8 +213,8 @@ class TSDRReq(object):
             self.getCSVData()
             if self.CSVDataIsValid:
                 self.getTSDRMap()
-        return 
-    
+        return
+
     def getXMLData(self, identifier=None, tmtype=None):
         '''
         Obtain XML trademark data from the PTO or local file
@@ -241,7 +241,7 @@ class TSDRReq(object):
         else:
             self.getXMLDataFromPTO(identifier, tmtype)
         return
-    
+
     def getXMLDataFromFile(self, filename):
         '''
         Load TDSR data from local file (which may be a zip file or text XML file).
@@ -260,13 +260,13 @@ class TSDRReq(object):
 
         with open(filename, "rb") as f:
             filedata = f.read()
-            
+
         _TSDR_substitutions["$XMLSOURCE$"] = filename
         now = time.strftime("%Y-%m-%d %H:%M:%S")
         _TSDR_substitutions["$EXECUTIONDATETIME$"] = now
 
         self._processFileContents(filedata)
-        return 
+        return
 
     def getXMLDataFromPTO(self, number, tmtype):
         '''
@@ -284,11 +284,11 @@ class TSDRReq(object):
             self.XMLData
             self.ZipData (for format="zip")
             self.ImageThumb (for format="zip")
-            self.ImageFull (for format="zip")           
+            self.ImageFull (for format="zip")
         '''
 
         self._validate_PTO_parameters(number, tmtype)
-        
+
         xml_url_template_st66 = "https://tsdrapi.uspto.gov/ts/cd/status66/"\
                                 "%sn%s/info.xml"
         xml_url_template_st96 = "https://tsdrapi.uspto.gov/ts/cd/casestatus/"\
@@ -307,7 +307,7 @@ class TSDRReq(object):
         ##  with urllib2.urlopen(pto_url) as f:  ## This doesn't work; in Python 2.x,
         ##      filedata = f.read()              ## urlopen() does not support the "with" statement
         ## I'm only leaving this comment here because twice I've forgotten that this won't work
-        ## in Python 2.7, and attempt the "with" statement before it bites me and I remember. 
+        ## in Python 2.7, and attempt the "with" statement before it bites me and I remember.
         try:
             ### PEP 476:
             ### use context paramenter if it is supported (TypeError if not)
@@ -323,16 +323,16 @@ class TSDRReq(object):
                 return
             else:
                 raise
-                        
+
         filedata = f.read()
         f.close()
-        
+
         _TSDR_substitutions["$XMLSOURCE$"] = pto_url
         now = time.strftime("%Y-%m-%d %H:%M:%S")
         _TSDR_substitutions["$EXECUTIONDATETIME$"] = now
 
         self._processFileContents(filedata)
-        return 
+        return
 
     def getCSVData(self):
         '''
@@ -344,7 +344,7 @@ class TSDRReq(object):
 
         Either getTDSRDataFromPTO or getTDSRDataFromFile must be
         successfully invoked before using this method.
-        
+
         Parameters: none.
 
         Reads:
@@ -353,14 +353,14 @@ class TSDRReq(object):
             self.CSVData
         '''
         self.resetCSVData()  # Clear out any data from prior use
-        
+
         # First, make sure there is XML data to process
         if not self.XMLDataIsValid:
             self.CSVDataIsValid = False
             self.ErrorCode = "CSV-NoValidXML"
             self.ErrorMessage = "No valid XML data found"
             return
-        
+
         # Prep the XML
         f = StringIO.StringIO(self.XMLData)
         parsed_xml = etree.parse(f)
@@ -382,7 +382,7 @@ class TSDRReq(object):
                     self.CSVDataIsValid = False
                     self.ErrorCode = "CSV-UnsupportedXML"
                     self.ErrorMessage = "Unsupported XML format found: %s" % xml_format
-                    return                    
+                    return
             xslt_transform_info = _xslt_table[xml_format]
             transform = xslt_transform_info.transform
             _TSDR_substitutions["$XSLTFILENAME$"] = xslt_transform_info.filename
@@ -391,7 +391,7 @@ class TSDRReq(object):
         transformed_tree = transform(parsed_xml)
         pairs = self._perform_substitution(str(transformed_tree))
         self.CSVData = pairs
-        
+
         (csvvalid, errcode, errmsg) = self._validateCSV()
         if csvvalid:
             self.CSVDataIsValid = True
@@ -431,13 +431,13 @@ class TSDRReq(object):
         # At end of processing, merge repeated-item dict into main dict.
         # First-found "Applicant" is deemed current applicant, so copy that one to
         # the main entry.
-        
+
         self.resetTSDRMap()     # Clear out any data from prior use
-        
+
         if not self.CSVDataIsValid:
             self.ErrorCode = "Map-NoValidCSV"
             self.ErrorMessage = "No valid CSV data"
-            return 
+            return
 
         repeated_item_dict = {}
         output_dict = {}
@@ -460,33 +460,32 @@ class TSDRReq(object):
                     repeated_item_dict[data+"List"] = []
                 repeated_item_dict[data+"List"].append(current_dict)
                 # done processing list, resume regular output
-                current_dict = output_dict    
+                current_dict = output_dict
             else:
                 current_dict[key] = data
         output_dict.update(repeated_item_dict)
         ### (This was a misguided idea, removed)
         ###
         ### First-listed applicant is current owner, so if it's there (and it
-        ### should be), promote it up to main list 
+        ### should be), promote it up to main list
         ### if "ApplicantList" in repeated_item_dict.keys():
         ###    primary_applicant_dict = repeated_item_dict["ApplicantList"][0]
         ###    output_dict.update(primary_applicant_dict)
         ###
         self.TSDRMap = output_dict
         self.TSDRMapIsValid = True
-        return 
-
+        return
 
     def _processFileContents(self, filedata):
         # At this point, we've read data, but don't know whether its XML or zip
         stringIOfile = StringIO.StringIO(filedata)
         if zipfile.is_zipfile(stringIOfile):
             # it's a zip file, process it as a zip file, pulling XML data, and other stuff, from the zip
-            self._processZip(stringIOfile, filedata)       
+            self._processZip(stringIOfile, filedata)
         else:
             # it's not a zip, it's assumed XML-only; just copy to XMLData (other fields will remain None)
             self.XMLData = filedata
-        
+
         error_reason = self._xml_sanity_check(self.XMLData)
         if error_reason != "":
             self.XMLDataIsValid = False
@@ -522,7 +521,7 @@ class TSDRReq(object):
         Validate parameters used to fetch from PTO.
 
             number: string made up of 7 or 8 digits;
-                7 digits for registrations, 8 for applications   
+                7 digits for registrations, 8 for applications
             tmtype: string;
                 "s" (serial number of application) or "r" (registation number).
         '''
@@ -570,18 +569,18 @@ class TSDRReq(object):
         except KeyError:
             pass
         self.ZipData = zipdata
-            
-        return 
+
+        return
 
     def _determine_xml_format(self, tree):
         '''
         Given a parsed XML tree, determine its format ("ST66" or "ST96";
         or None, if not determinable)
         '''
-        
+
         ST66_root_tag = \
             "{http://www.wipo.int/standards/XMLSchema/trademarks}Transaction"
-        ## Former tag value for ST-96 1_D3; keeping it around as known but unsupported for diagnostic value 
+        ## Former tag value for ST-96 1_D3; keeping it around as known but unsupported for diagnostic value
         ST96_1_D3_root_tag = \
             "{http://www.wipo.int/standards/XMLSchema/Trademark/1}Transaction"
         ST96_root_tag = \
@@ -607,7 +606,7 @@ class TSDRReq(object):
         survive the XSLT transform without errors, but potentially produce erroneous
         data that might cause hard-to-find problems downstream.
 
-        It checks:  
+        It checks:
          * CSV data consists of more than one line (a common error is a bad XSLT
            transform that slurps in entire file as one line, especially if using an
            XSLT transform designed for ST66 on ST96 data, or vice-versa).
@@ -651,7 +650,7 @@ class TSDRReq(object):
                     error_message = "getCSVData [line %s]: " \
                         "invalid key <%s> found (invalid characters in key)" \
                         % (line_number_offset+1, k)
-                    raise ValueError 
+                    raise ValueError
                 stripped_v = v[1:-1]
                 if '"' + stripped_v + '"' != v:
                     error_code = "CSV-InvalidValue"
@@ -659,7 +658,7 @@ class TSDRReq(object):
                         "invalid value <%s> found for key <%s> " \
                         "(does not begin and end with double-quote character)" \
                         % (line_number_offset+1, v, k)
-                    raise ValueError          
+                    raise ValueError
         except ValueError:
             if error_code == None:   # Not good, something we didn't count on went wrong
                 error_code = "CSV-UnknownError"
@@ -676,10 +675,10 @@ class TSDRReq(object):
         return s
 
 class TestUM(unittest.TestCase):
- 
+
     def setUp(self):
         pass
-            
+
     def test_00_vanilla(self):
         t = TSDRReq()
         t.getTSDRInfo("sn76044902.zip")
@@ -699,7 +698,7 @@ class TestUM(unittest.TestCase):
                 "A Sections 8 and 15 combined declaration has been accepted and acknowledged.")
         self.assertEqual(t.TSDRMap["MarkCurrentStatusDate"], "2010-09-08-04:00")
         self.assertEqual(t.TSDRMap["MarkCurrentStatusDate"][0:10], t.TSDRMap["MarkCurrentStatusDateTruncated"])
-        applicant_info = t.TSDRMap["ApplicantList"][0]    
+        applicant_info = t.TSDRMap["ApplicantList"][0]
         self.assertEqual(applicant_info["ApplicantName"], "PYTHON SOFTWARE FOUNDATION")
         assignment = t.TSDRMap["AssignmentList"][0]
         self.assertEqual(assignment["AssignorEntityName"],
